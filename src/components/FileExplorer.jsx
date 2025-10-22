ee0b4517109a2b0f29158b5cdbc2508c8dc989f1
akashwadode <ajwadode25@gmail.com>
2025-10-22 07:20:54 +0530

    feat: sandpack-file-explporer implement with file management functionality

--- src/components/FileExplorer.jsx
+++ src/components/FileExplorer.jsx
@@ -1,5 +1,4 @@
-import { SandpackFileExplorer } from "@codesandbox/sandpack-react";
-
+import { SandpackFileExplorer } from 'sandpack-file-explorer'
 export default function FileExplorer({ theme }) {
   return (
     <div className="file-explorer">
